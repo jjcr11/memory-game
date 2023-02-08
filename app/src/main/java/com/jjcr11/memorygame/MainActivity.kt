00b2794a604aaef7b147c7a3437037cc3dd58925
jjcr11 <jjcr0397@gmail.com>
2023-02-08 10:44:47 -0600

    Project setup

---
+++ app/src/main/java/com/jjcr11/memorygame/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.jjcr11.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jjcr11.memorygame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
