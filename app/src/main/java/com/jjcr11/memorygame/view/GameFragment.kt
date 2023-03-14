e656ba8842e8ca2e2aaf049a0c83d47ceab5426a
jjcr11 <jjcr0397@gmail.com>
2023-03-14 16:54:58 -0600

    Fix gameplay

--- app/src/main/java/com/jjcr11/memorygame/view/GameFragment.kt
+++ app/src/main/java/com/jjcr11/memorygame/view/GameFragment.kt
@@ -1,7 +1,5 @@
 package com.jjcr11.memorygame.view
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
@@ -11,12 +9,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.google.android.material.card.MaterialCardView
 import com.jjcr11.memorygame.R
 import com.jjcr11.memorygame.databinding.FragmentGameBinding
 import com.jjcr11.memorygame.model.AppDatabase
@@ -31,15 +27,10 @@ import kotlin.math.hypot
 class GameFragment : Fragment() {
 
     private lateinit var binding: FragmentGameBinding
-    private lateinit var mcvMain: MaterialCardView
-    private lateinit var mcvMain2: MaterialCardView
-    private lateinit var tvMain: TextView
-    private lateinit var tvMain2: TextView
     private lateinit var sharedPreferences: SharedPreferences
     private var round = 1
     private var size = 3
     private val gameColors = mutableListOf<String>()
-    private val userColors = mutableListOf<String>()
     private var score = 0
 
     private lateinit var colorButton1: String
@@ -57,12 +48,6 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentGameBinding.inflate(inflater, container, false)
-
-        mcvMain = binding.mcvMain
-        mcvMain2 = binding.mcvMain2
-
-        tvMain = binding.tvMain
-        tvMain2 = binding.tvMain2
 
         sharedPreferences = activity?.getSharedPreferences("settings", Context.MODE_PRIVATE)!!
 
@@ -88,61 +73,86 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
-    private fun startGame() {
-        val colors = mutableListOf(
-            colorButton1,
-            colorButton2,
-            colorButton3,
-            colorButton4,
-            colorButton5,
-            colorButton6,
-            colorButton7,
-            colorButton8
-        )
-
-        if (round % 5 == 0) {
-            size++
-        }
-
+    private fun startGame(delay: Long = 0) {
         lifecycleScope.launch(Dispatchers.Main) {
+            disabledAll()
+            delay(delay)
+            binding.mcvMain.visibility = View.INVISIBLE
             binding.mcvPlay.visibility = View.INVISIBLE
             binding.tvScore.visibility = View.VISIBLE
 
-            mcvMain.visibility = View.INVISIBLE
-            mcvMain2.visibility = View.INVISIBLE
-            delay(1000)
+            val colors = mutableListOf(
+                colorButton1,
+                colorButton2,
+                colorButton3,
+                colorButton4,
+                colorButton5,
+                colorButton6,
+                colorButton7,
+                colorButton8
+            )
+
+            if (round % 5 == 0) {
+                size++
+            }
 
             for (i in 1..size) {
                 val color = colors.random()
-                val animation = setupAnimation(color)
-                animation.start()
-                mcvMain2.visibility = View.VISIBLE
+
+                if (sharedPreferences.getInt("type", 0) != 1) {
+                    binding.mcvMain.setBackgroundColor(Color.parseColor(color))
+                }
+
+                binding.tvMain.text = getNumber(color)
+                startAnimation()
+
+                binding.mcvMain.visibility = View.VISIBLE
                 gameColors.add(color)
                 colors.remove(color)
-                delay(700)
+                delay(900)
+                binding.mcvMain.visibility = View.INVISIBLE
             }
-
-            mcvMain.visibility = View.INVISIBLE
-            mcvMain2.visibility = View.INVISIBLE
+            enabledAll()
         }
     }
 
     private fun changeColor(color: String) {
-        val animation = setupAnimation(color)
-        animation.start()
-        mcvMain2.visibility = View.VISIBLE
-        userColors.add(color)
-    }
-
-    private fun setupAnimation(color: String): Animator {
-        mcvMain.elevation = 1f
-        mcvMain2.elevation = 200f
-
         if (sharedPreferences.getInt("type", 0) != 1) {
-            mcvMain2.setBackgroundColor(Color.parseColor(color))
+            binding.mcvMain.setBackgroundColor(Color.parseColor(color))
         }
 
-        tvMain2.text = when(color) {
+        binding.tvMain.text = getNumber(color)
+        startAnimation()
+
+        binding.mcvMain.visibility = View.VISIBLE
+
+        if (color == gameColors[0]) {
+            gameColors.remove(color)
+            if (gameColors.isEmpty()) {
+                winRound()
+            }
+        } else {
+            failRound()
+        }
+
+    }
+
+    private fun startAnimation() {
+        val coords = Pair(binding.mcvMain.width / 2, binding.mcvMain.height / 2)
+        val radius = hypot(coords.first.toDouble(), coords.second.toDouble()).toFloat()
+        val animation = ViewAnimationUtils.createCircularReveal(
+            binding.mcvMain,
+            coords.first,
+            coords.second,
+            0f,
+            radius
+        )
+        animation.duration = 500
+        animation.start()
+    }
+
+    private fun getNumber(color: String): String {
+        return when (color) {
             colorButton1 -> "1"
             colorButton2 -> "2"
             colorButton3 -> "3"
@@ -153,62 +163,22 @@ class GameFragment : Fragment() {
             colorButton8 -> "8"
             else -> ""
         }
-
-        val coords = Pair(mcvMain2.width / 2, mcvMain2.height / 2)
-        val radius = hypot(coords.first.toDouble(), coords.second.toDouble()).toFloat()
-        val animation = ViewAnimationUtils.createCircularReveal(
-            mcvMain2,
-            coords.first,
-            coords.second,
-            0f,
-            radius
-        ).let {
-            it.addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                    val mcvAux = mcvMain
-                    mcvMain = mcvMain2
-                    mcvMain2 = mcvAux
-                    mcvMain2.visibility = View.INVISIBLE
-
-                    val tvAux = tvMain
-                    tvMain = tvMain2
-                    tvMain2 = tvAux
-
-                    if (userColors.size == size) {
-                        if (userColors == gameColors) {
-                            winRound()
-                        } else {
-                            failGame()
-                        }
-                        gameColors.clear()
-                        userColors.clear()
-                    }
-                    if (gameColors.size == size) {
-                        enabledAll()
-                    }
-                }
-            })
-            it.duration = 500
-            it
-        }
-        return animation
     }
+
 
     private fun winRound() {
         score++
-        binding.tvScore.text = score.toString()
         round++
-        startGame()
+        gameColors.clear()
+        binding.tvScore.text = "$score"
+        startGame(delay = 1000)
     }
 
-    private fun failGame() {
+    private fun failRound() {
         lifecycleScope.launch(Dispatchers.Main) {
             val dao = AppDatabase.getDatabase(requireContext()).dao()
             withContext(Dispatchers.IO) {
-                val sharedPreferences =
-                    activity?.getSharedPreferences("settings", Context.MODE_PRIVATE)
-                val underScore = sharedPreferences?.getFloat("underScore", 0f)!!
+                val underScore = sharedPreferences.getFloat("underScore", 0f)
                 if (score >= underScore) {
                     val date = Date()
                     val score = Score(
@@ -229,13 +199,14 @@ class GameFragment : Fragment() {
                     }
                 }
             }
-            Toast.makeText(requireContext(), "Game Over", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Score: $score", Toast.LENGTH_LONG).show()
+            binding.mcvPlay.visibility = View.VISIBLE
+            binding.tvScore.visibility = View.INVISIBLE
             score = 0
             round = 1
             size = 3
-            binding.tvScore.text = score.toString()
-            binding.tvScore.visibility = View.INVISIBLE
-            binding.mcvPlay.visibility = View.VISIBLE
+            gameColors.clear()
+            binding.tvScore.text = "$score"
             disabledAll()
         }
     }
@@ -263,8 +234,7 @@ class GameFragment : Fragment() {
     }
 
     private fun setAllNumbersAsInvisible() {
-        tvMain.visibility = View.INVISIBLE
-        tvMain2.visibility = View.INVISIBLE
+        binding.tvMain.visibility = View.INVISIBLE
         binding.tvButton1.visibility = View.INVISIBLE
         binding.tvButton2.visibility = View.INVISIBLE
         binding.tvButton3.visibility = View.INVISIBLE
@@ -289,7 +259,7 @@ class GameFragment : Fragment() {
 
     private fun setColorButtons() {
         val colors: SharedPreferences
-        if(isDarkTheme()) {
+        if (isDarkTheme()) {
             colors = activity?.getSharedPreferences("darkColors", Context.MODE_PRIVATE)!!
             colorButton1 = colors.getString("colorButton1", "#DB070A")!!
             colorButton2 = colors.getString("colorButton2", "#C14B0B")!!
