61a25339a8a0268da25194834505fea9e2395e97
jjcr11 <jjcr0397@gmail.com>
2023-03-23 12:18:20 -0600

    Add layout for fold phones

--- app/src/main/java/com/jjcr11/memorygame/view/ScoreFragment.kt
+++ app/src/main/java/com/jjcr11/memorygame/view/ScoreFragment.kt
@@ -2,6 +2,7 @@ package com.jjcr11.memorygame.view
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -90,13 +91,8 @@ class ScoreFragment : Fragment(), ScoreAdapterOnClick {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val metrics = WindowMetricsCalculator.getOrCreate()
-            .computeCurrentWindowMetrics(requireActivity())
-
-        val dp = metrics.bounds.height() /
-                resources.displayMetrics.density
-
-        if (dp >= 480) {
+        val width = resources.displayMetrics.widthPixels / resources.displayMetrics.density
+        if(width < 600) {
             swipeHelper.attachToRecyclerView(binding.rv)
         }
 
